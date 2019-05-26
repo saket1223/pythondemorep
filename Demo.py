bc670f2ad95bf1f317a6405a42a83bd4a853b0f7
SUNISHKA YADAV <saket12_23@yahoo.co.in>
2019-05-26 15:41:18 +0530

    Update showing found position

--- Demo.py
+++ Demo.py
@@ -1,15 +1,18 @@
+pos = -1
 def search(list,n):
-  for i in len(list):
-    if list[i]==list[n]:
-      return True;
-    else:
-      return False;
+  i = 0
+  while i < len(list):
+    if list[i]== n :
+      globals()['pos'] = i
+      return True
+    i+=1
+  return False
       
      
      
 list =[2,4,5,6,7,81,3]
-n=5
+n=2
 if search(list,n):
-  print('Found');
+  print('Found at position ',pos+1 )
 else:
   print('Not found')
